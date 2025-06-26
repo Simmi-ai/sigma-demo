16774c22b755eeef528f7b7ca73dcadfbdae7e8b
Simmi Kumari <simmi6166@gmail.com>
2025-06-26 19:10:53 +0530

    Add New File

--- app.js
+++ app.js
@@ -97,8 +97,14 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080,() => {
-    console.log("server is listening on port 8080");
+// app.listen(8080,() => {
+//     console.log("server is listening on port 8080");
+// });
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 
